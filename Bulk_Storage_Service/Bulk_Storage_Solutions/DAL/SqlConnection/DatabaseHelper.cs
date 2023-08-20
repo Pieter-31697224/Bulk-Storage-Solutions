96b301c4d60febf6684aa6ef051602a227597351
Pieter-Coetzee <pieterc@zonketech.co.za>
2023-08-20 17:50:45 +0200

    Added global connection string in web config gile. Created DAL (Database Access Layer) with global connection string. Structured all pages within Views folder.

---
+++ Bulk_Storage_Service/Bulk_Storage_Solutions/DAL/SqlConnection/DatabaseHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Bulk_Storage_Solutions.DAL.SqlConnection
+{
+    public class DatabaseHelper
+    {
+        private string connectionString;
+
+        public DatabaseHelper() : this(ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString)
+        {
+
+        }
+
+        public DatabaseHelper(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    }
+}
