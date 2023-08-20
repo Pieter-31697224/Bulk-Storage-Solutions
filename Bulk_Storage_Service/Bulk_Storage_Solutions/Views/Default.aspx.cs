4e645de935025244a484a094b736fd78ee91353a
Pieter-Coetzee <pieterc@zonketech.co.za>
2023-08-20 20:32:09 +0200

    Implemented structured approach to solution. Added contracts table to default view.

--- Bulk_Storage_Service/Bulk_Storage_Solutions/Views/Default.aspx.cs
+++ Bulk_Storage_Service/Bulk_Storage_Solutions/Views/Default.aspx.cs
@@ -1,17 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Bulk_Storage_Solutions.DAL.Features.Contracts;
+using System;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Bulk_Storage_Solutions
 {
     public partial class _Default : Page
     {
+        private readonly IContracts _contracts;
+
+        public _Default()
+        {
+            _contracts = Global.Resolve<IContracts>();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                ContractsGridView.DataSource = _contracts.GetAllRowsFromContracts();
+                ContractsGridView.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
