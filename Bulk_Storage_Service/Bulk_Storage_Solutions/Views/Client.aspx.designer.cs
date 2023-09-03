1317a5d96537d94a0bae2b3acc7b40f7f3de0beb
Pieter-Coetzee <pieterc@zonketech.co.za>
2023-09-03 20:38:22 +0200

    Deleted login and sign up functionality. Fixed clients functionality

--- Bulk_Storage_Service/Bulk_Storage_Solutions/Views/Client.aspx.designer.cs
+++ Bulk_Storage_Service/Bulk_Storage_Solutions/Views/Client.aspx.designer.cs
@@ -123,15 +123,6 @@ namespace Bulk_Storage_Solutions.Views
         protected global::System.Web.UI.WebControls.HiddenField EditClientID;
 
         /// <summary>
-        /// txtEditClientId control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtEditClientId;
-
-        /// <summary>
         /// txtEditClientStatus control.
         /// </summary>
         /// <remarks>
